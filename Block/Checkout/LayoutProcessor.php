fde52307f9ba730f630380dcc3276562d1d10196
verimod <admin@verimod.com.tr>
2026-06-05 18:41:01 +0300

    feat: Add checkout layout processor block

---
+++ Block/Checkout/LayoutProcessor.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Verimod Tax Office Module - Checkout Layout Processor Block
+ *
+ * @category    Verimod
+ * @package     Verimod_TaxOffice
+ */
+
+namespace Verimod\TaxOffice\Block\Checkout;
+
+class LayoutProcessor
+{
+    /**
+     * Process layout for checkout
+     *
+     * @param array $jsLayout
+     * @return array
+     */
+    public function process(array $jsLayout)
+    {
+        return $jsLayout;
+    }
+}
